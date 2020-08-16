a03bb31155e9360bd36ec0e01cd4f881f0604d88
vertuli <michael@vertuli.com>
2020-08-16 11:08:42 -0700

    PingsGetter now writes pings to local JSON lines files.

--- spark/README.md
+++ spark/README.md
@@ -2,6 +2,10 @@
 
 Getting pings from http://api.metro.net and using Spark's structured streaming to create a history of vehicle paths.
 
-`mill metro.pings.test` - runs pings tests
-`mill metro.paths.test` - runs paths tests
+## pings
+- `mill metro.pings.run` - gets pings from `api.metro.net` for the agencies `lametro` and `lametro-rail` and writes them as JSON lines to `data/pings/`, partitioned by `agency_id` and `localdate`.
+- `mill metro.pings.test` - runs pings tests
+
+## paths
+- `mill metro.paths.test` - runs paths tests
 
--- spark/metro/pings/src/PingsGetter.scala
+++ spark/metro/pings/src/PingsGetter.scala
@@ -7,11 +7,12 @@ import java.time.ZoneOffset.UTC
 object PingsGetter {
 
 	val agency_ids = Seq("lametro", "lametro-rail")
+	val pingsPath = os.pwd / "data" / "pings"
 
 	def main(args: Array[String]): Unit = {
 		for (agency_id <- agency_ids) {
 			val (pings, dt) = get(agency_id)
-			val path = os.pwd / "data" / "pings" / s"agency_id=$agency_id" / s"response_ts=${dt.toEpochSecond(UTC)}" / "pings.json"
+			val path = pingsPath / s"agency_id=$agency_id" / s"local_date=${dt.toLocalDate}" / s"${dt.toEpochSecond(UTC)}.json"
 			os.write(path, pings.mkString("\n"), createFolders=true)
   	}
 	}
@@ -19,9 +20,12 @@ object PingsGetter {
 	def get(agency_id: String): (Seq[String], LocalDateTime) = {
 		val r = requests.get(s"http://api.metro.net/agencies/$agency_id/vehicles/")
 		val data = ujson.read(r.text)
-		val pings = for (item <- data("items").arr) yield ujson.write(item)
-		val dt = LocalDateTime.parse(r.headers("date")(0), RFC_1123_DATE_TIME)
-		(pings, dt)
+		val responseDt = LocalDateTime.parse(r.headers("date")(0), RFC_1123_DATE_TIME)
+		val pings = for (item <- data("items").arr) yield {
+			item("event_ts") = ujson.Num(responseDt.minusSeconds(item("seconds_since_report").num.toLong).toEpochSecond(UTC))
+			ujson.write(item)
+		}
+		(pings, responseDt)
 	}
 }
 
