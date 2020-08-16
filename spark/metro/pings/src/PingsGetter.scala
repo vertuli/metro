f49d37b28e6cf70929b44a8949c003cbc2751b18
vertuli <michael@vertuli.com>
2020-08-16 16:16:19 -0700

    PingsGetter runs every minute indefinitely

--- spark/metro/pings/src/PingsGetter.scala
+++ spark/metro/pings/src/PingsGetter.scala
@@ -5,24 +5,33 @@ import java.time.format.DateTimeFormatter.RFC_1123_DATE_TIME
 import java.time.ZoneOffset.UTC
 
 object PingsGetter {
-
 	val agency_ids = Seq("lametro", "lametro-rail")
-	val pingsPath = os.pwd / "data" / "pings"
+	val pingsBasePath = os.pwd / "data" / "pings"
 
 	def main(args: Array[String]): Unit = {
-		for (agency_id <- agency_ids) {
-			val (pings, dt) = get(agency_id)
-			val path = pingsPath / s"agency_id=$agency_id" / s"local_date=${dt.toLocalDate}" / s"${dt.toEpochSecond(UTC)}.json"
-			os.write(path, pings.mkString("\n"), createFolders=true)
-  	}
+		println("Starting...")
+		while(true) {
+			for (agency_id <- agency_ids) {
+				get(agency_id)  // TODO: handle errors from metro API
+			}
+			Thread.sleep(60000)
+		}
 	}
 
-	def get(agency_id: String): (Seq[String], LocalDateTime) = {
+	def get(agency_id: String): Unit = {
+		val (pings, dt) = request(agency_id)
+		val subPath = os.SubPath(s"agency_id=$agency_id/local_date=${dt.toLocalDate}/${dt.toEpochSecond(UTC)}.json")
+		os.write(pingsBasePath / subPath, pings.mkString("\n"), createFolders=true)
+		println(s"$dt - ${pings.length} pings written to $subPath")  // TODO: logging
+	}
+
+	def request(agency_id: String): (Seq[String], LocalDateTime) = {
 		val r = requests.get(s"http://api.metro.net/agencies/$agency_id/vehicles/")
 		val data = ujson.read(r.text)
 		val responseDt = LocalDateTime.parse(r.headers("date")(0), RFC_1123_DATE_TIME)
 		val pings = for (item <- data("items").arr) yield {
-			item("event_ts") = ujson.Num(responseDt.minusSeconds(item("seconds_since_report").num.toLong).toEpochSecond(UTC))
+			val eventDt = responseDt.minusSeconds(item("seconds_since_report").num.toLong)
+			item("event_ts") = ujson.Num(eventDt.toEpochSecond(UTC))
 			ujson.write(item)
 		}
 		(pings, responseDt)
